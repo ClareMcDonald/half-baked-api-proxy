bc758cdee408cbac41e07feabe64438838c2234c
ClareMcDonald <clare.s.mcdonald@gmail.com>
2022-02-14 17:15:06 -0800

    pokemon can be found! now need to do react work

--- functions/pokemon.js
+++ functions/pokemon.js
@@ -5,8 +5,8 @@ require('dotenv').config();
 exports.handler = async (event, context) => {
   try {
     // grab the pokemon's name from the request's query parameters
-    const response = await fetch(`https://pokedex-alchemy.herokuapp.com/api/pokedex/search?pokemon=${event.queryStringParameters.search}`);
-
+    const response = await fetch(`https://pokedex-alchemy.herokuapp.com/api/pokedex?pokemon=${event.queryStringParameters.search}`);
+    console.log(response);
     const json = await response.json();
 
     // here is an example from the netlify docs:
@@ -18,7 +18,7 @@ exports.handler = async (event, context) => {
     return { 
       statusCode: 200, 
     // this is where you shoot data back to the user. right now it's sending an empty object--replace this with the pokemon data. remember, you do need to stringify it, otherwise netlify gets mad. ¯\_(ツ)_/¯
-      body: JSON.stringify(json.pokemon),
+      body: JSON.stringify(json.results),
     };
   } catch (error) {
     console.log(error);
--- src/PokemonSearch.js
+++ src/PokemonSearch.js
@@ -6,8 +6,7 @@ export default function PokemonSearch() {
       // you'll need to track your pokemon search results, the loading state, and one form field: name. For this form field, set a real initial values (like 'pikachu') so the form populates with a default value.
   const [pokemon, setPokemon] = useState([]);
   const [loading, setLoading] = useState(false);
-  const [search, setSearch] = useState('');
-  const [searchByName, setSearchByName] = useState('pikachu');
+  const [searchName, setSearchName] = useState('pikachu');
 
   
   async function handlePokemonSubmit(e) {
@@ -17,12 +16,12 @@ export default function PokemonSearch() {
     setLoading(true);
 
         // use fetch to make a request to your netlify pokemon function. Be sure to pass the pokemon name as a query param in the URL
-    const endpointURL = `https://pokedex-alchemy.herokuapp.com/api/pokedex?${searchByName}=${search}`;
 
-    const response = await fetch(endpointURL);
+    const response = await fetch(`/.netlify/functions/pokemon?search=${searchName}`);
 
         // put the jsonified data in state and set the loading state to false
     const json = await response.json();
+
     setPokemon(json.results);
 
     setLoading(false);
@@ -34,15 +33,13 @@ export default function PokemonSearch() {
       <form onSubmit={handlePokemonSubmit}>
             Search pokemon for a name
         {/* add inputs/labels for name, using all the things we need with react forms. Don't forget to use the value property to sync these up with the default values in react state */}
-        <input onChange={e => setSearch(e.target.value)} />
-        <select onChange={e => setSearchByName(e.target.value)} />
-        <option value='pokemon'>Search By Name</option>
+        <input onChange={e => setSearchName(e.target.value)} />
         <button>Get pokemon</button>
       </form>
       {/* Make a PokemonList component to import and use here. Use a ternery to display a loading spinner (make a <Spinner /> component for this) if the data is still loading. */}
-      if {!loading}
-      ? <PokemonList pokemon={pokemon} />
-      : <Spinner />
+      {/* if {!loading} */}
+      <PokemonList pokemon={pokemon} />
+      {/* : <Spinner /> */}
     </section>
   );
 
