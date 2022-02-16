331ba5855eab6f40ef654c302e2c576d2d62f9ac
ClareMcDonald <clare.s.mcdonald@gmail.com>
2022-02-15 16:44:50 -0800

    mapped through businesses in BusinessesList

--- src/BusinessesList.js
+++ src/BusinessesList.js
@@ -1,7 +1,14 @@
 import React from 'react';
 
-export default function BusinessesList() {
+export default function BusinessesList({ businesses }) {
   return (
-    <div>BusinessesList</div>
+    <div>
+      {businesses.map((business, i) => <div key={business + i}>
+        <h3>{business.name}</h3>
+        <img src={business.image_url} />
+        <p>Rating: {business.rating}</p>
+        <p>Price: {business.price}</p>
+      </div>)}
+    </div>
   );
 }
